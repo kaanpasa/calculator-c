bb3113fc9def5154fd484c737fcc54d6a96dcef3
Kaan Pasa <45922962+kaanpasa@users.noreply.github.com>
2020-12-27 23:37:03 +0100

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int number1;
+	int number2;
+	char sign;
+	int total;
+	
+	printf("Enter Calculation : ");
+	scanf("%d %c %d", &number1, &sign, &number2);
+	if(sign == '+')
+		{
+			total = number1 + number2;
+			printf("%d %c %d = %d", number1, sign, number2, total);
+		}
+	else if(sign == '-')
+		{
+			total = number1 - number2;
+			printf("%d %c %d = %d", number1, sign, number2, total);
+		}
+	else if(sign == '*')
+		{
+			total = number1 * number2;
+			printf("%d %c %d = %d", number1, sign, number2, total);
+		}
+	else if(sign == '/')
+		{
+			total = number1 / number2;
+			printf("%d %c %d = %d", number1, sign, number2, total);
+		}
+return 0;
+}
